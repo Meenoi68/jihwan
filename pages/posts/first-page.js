48611b128aa502595cc4cf7c0ee5ae5d354573cf
Meenoi68 <bluekenn03@hanyang.ac.kr>
2023-04-18 15:41:23 +0900

    p5 js uploaded

---
+++ pages/posts/first-page.js
@@ -0,0 +1,19 @@
+export default function FirstPage() 
+{
+    return (
+        <><div>
+            <h1>Jihwan's web page</h1>
+        </div><div>
+            <h2>My Birthday</h2>
+            <h5>2003(Y) 10(M) 20(D)</h5>
+            </div><div>
+                <h2>My hobby</h2>
+                <ul>
+                    <li>Computer game</li>
+                    <li>Basketball</li>
+                    <li>Bicycle</li>
+                </ul>
+            </div></>
+        
+    )
+}
